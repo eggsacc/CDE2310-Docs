90eaa9bdcb274ffd9f3b4215de61b192b64d2d6f
eggsacc <yizhangwang07@gmail.com>
2026-04-07 00:49:02 +0800

    docs(ros2-package):fix file hierarchy in ros2-package. Update main README to reflect change.

--- ros2-package/auto_nav/auto_nav/setup.py
+++ ros2-package/auto_nav/auto_nav/setup.py
@@ -29,8 +29,6 @@ setup(
             'exploration = auto_nav.exploration:main',
             'fsm_controller = auto_nav.fsm_controller:main',
             'docking = auto_nav.docking:main',
-            
-
         ],
     },
 )
